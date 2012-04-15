ba025aee2afe2bc3f45f9079a658d08bb412aaab
Björn Schmitz <profbulb@googlemail.com>
2012-04-15 23:38:45 +0200

    MiniDateCalendarPanel updated.

--- src/main/java/com/googlecode/commons/swing/demo/component/datetime/MiniDateCalendarPanel.java
+++ src/main/java/com/googlecode/commons/swing/demo/component/datetime/MiniDateCalendarPanel.java
@@ -4,21 +4,28 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import org.apache.commons.lang3.time.DateFormatUtils;
 
 import com.googlecode.commons.swing.component.datetime.MiniDateCalendar;
 
-public class MiniDateCalendarPanel extends MiniDateCalendar {
+public class MiniDateCalendarPanel extends JPanel {
 
+	private MiniDateCalendar calendar;
+	
 	public MiniDateCalendarPanel() {
 		super();
-		addActionListener(new ActionListener() {
+		calendar = new MiniDateCalendar();
+		calendar.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(MiniDateCalendarPanel.this, DateFormatUtils.format(getValue(), "dd.MM.yyyy HH:mm:ss"));
+				JOptionPane.showMessageDialog(MiniDateCalendarPanel.this, DateFormatUtils.format(calendar.getValue(), "dd.MM.yyyy HH:mm:ss"));
 			}
 		});
+		add(calendar);
+		
+		
 	}
 
 }
